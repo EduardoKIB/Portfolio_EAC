108103b021fa60f9dfaa7b84fc530f7ad1225022
Eduardo <eduardo_canhola@estudante.sesisenai.org.br>
2024-11-13 15:47:14 -0300

    portfolio quase pronto

--- Portfolio 2024/materias/humanas.js
+++ Portfolio 2024/materias/humanas.js
@@ -1,13 +1,13 @@
 document.getElementById('btnPrimeiro').addEventListener('click', function() {
     document.getElementById('atv-pri-trimestre').style.display = 'block';
     document.getElementById('atv-seg-trimestre').style.display = 'none';
-    document.getElementById('mensagem').style.display = 'none';
+    document.getElementById('atv-ter-trimestre').style.display = 'none';
 });
 
 document.getElementById('btnSegundo').addEventListener('click', function() {
     document.getElementById('atv-pri-trimestre').style.display = 'none';
     document.getElementById('atv-seg-trimestre').style.display = 'block';
-    document.getElementById('mensagem').style.display = 'none';
+    document.getElementById('atv-ter-trimestre').style.display = 'none';
 });
 
 document.getElementById('btnTerceiro').addEventListener('click', function() {
